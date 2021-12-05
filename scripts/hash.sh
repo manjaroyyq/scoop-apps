aec420a09d911979294a1f39a51d647463bf2c18
JaimeZeng <zxj@outlook.sg>
2021-12-05 18:02:37 +0800

    qq: add qq scripts
    Signed-off-by: JaimeZeng <zxj@outlook.sg>

---
+++ scripts/hash.sh
@@ -0,0 +1,15 @@
+#!/usr/sh
+
+cd $@
+# DOS to Unix
+vim install.bat -c "set ff=unix" -c ":wq"
+vim uninstall.bat -c "set ff=unix" -c ":wq"
+sha256sum *.bat
+
+# Unix to DOS
+# sed -i 's/$/\r/' $@
+# vim install.bat -c "set ff=dos" -c ":wq"
+# vim uninstall.bat -c "set ff=dos" -c ":wq"
+# sha256sum *.bat
+
+cd ..
